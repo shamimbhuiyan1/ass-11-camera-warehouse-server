c063c782f0856a0d162bd6b9715183a50d6c8028
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-20 17:55:19 +0600

    post,put delete created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -16,22 +17,52 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 async function run() {
   try {
     await client.connect();
     const productCollection = client.db("cameraStore").collection("product");
     console.log("db connected");
+
     app.get("/product", async (req, res) => {
       const query = {};
       const cursor = productCollection.find(query);
       const product = await cursor.toArray();
       res.send(product);
+
+      app.post("/inventory", async (req, res) => {
+        const newProduct = req.body;
+        console.log("Adding new user", newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+      });
+
+      // update user
+      app.put("/inventory/:id", async (req, res) => {
+        const id = req.params.id;
+        const updatedProduct = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updatedDoc = {
+          $set: {
+            name: updatedProduct.name,
+            email: updatedProduct.email,
+          },
+        };
+        const result = await productCollection.updateOne(
+          filter,
+          updatedDoc,
+          options
+        );
+        res.send(result);
+      });
+
+      //delete a user from server side
+      app.delete("/inventory/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+      });
     });
   } finally {
   }
