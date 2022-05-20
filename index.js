a2ba8a75a1a0ba42c318326744aebc6f77b2fd4f
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-20 17:30:53 +0600

    basic npm insall and mongodb connected

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+//using middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb
+const uri = `mongodb+srv://product:NumxIVcbkwG9GD0z@cluster0.oer4d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("cameraStore").collection("product");
+    console.log("db connected");
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const product = await cursor.toArray();
+      res.send(product);
+    });
+  } finally {
+  }
+}
+setTimeout(() => {
+  client.close();
+}, 1500);
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("This is Online Camera Warehouse.");
 });
